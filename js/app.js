e124eb7ad7efbe84ee3ed8afa5cca24cf00d6ad1
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-09-15 14:09:57 +0700

    Bg color and Resturant tap
    add card form json for Resturant tap
    add Bg colr
    add usable login

--- js/app.js
+++ js/app.js
@@ -10,6 +10,24 @@ document.addEventListener('init', function (event) {
         });
     }
 
+    if (page.id === "sidemenu") {
+        //Code for sidemenu
+        $("#regisbtn").click(function () {
+            console.log('regisbtn pressed');
+            var content = document.getElementById('content');
+            var menu = document.getElementById('menu');
+            content.load('regis.html')
+                .then(menu.close.bind(menu));
+        });
+        $("#loginbtn").click(function () {
+            console.log('loginbtn pressed');
+            var content = document.getElementById('content');
+            var menu = document.getElementById('menu');
+            content.load('login.html')
+                .then(menu.close.bind(menu));
+        });
+
+    }
 
     ///////////////////change tabbar color//////////////////
     $("#tabbar1").click(function () {
@@ -18,7 +36,7 @@ document.addEventListener('init', function (event) {
         $("#tabbar3").attr("style", "background-color: rgb(255, 163, 26);");
         $("#tabbar4").attr("style", "background-color: rgb(255, 163, 26);");
         $("#title").empty();
-        $("#title").append("Resturant Food");
+        $("#title").append("Popular");
     });
     $("#tabbar2").click(function () {
         $(this).attr("style", "background-color: rgb(173, 232, 105);");
@@ -46,24 +64,7 @@ document.addEventListener('init', function (event) {
     });
     ///////////////////end of change tabbar color//////////////////
 
-    if (page.id === "sidemenu") {
-        //Code for sidemenu
-        $("#regisbtn").click(function () {
-            console.log('regisbtn pressed');
-            var content = document.getElementById('content');
-            var menu = document.getElementById('menu');
-            content.load('regis.html')
-                .then(menu.close.bind(menu));
-        });
-        $("#loginbtn").click(function () {
-            console.log('loginbtn pressed');
-            var content = document.getElementById('content');
-            var menu = document.getElementById('menu');
-            content.load('login.html')
-                .then(menu.close.bind(menu));
-        });
 
-    }
 
     $("#backbtn").click(function () {
         document.querySelector('ons-navigator').resetToPage('splitter.html');
@@ -78,16 +79,24 @@ document.addEventListener('init', function (event) {
 
     $("#signinbtn").click(function () {
         console.log("signinbtn pressed");
-        document.querySelector('ons-navigator').resetToPage('splitter.html');
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+      
+        if (username === 'Admin' && password === '123') {
+          document.querySelector('ons-navigator').resetToPage('splitter.html');
+        } else {
+          ons.notification.alert('Incorrect username or password.');
+        }
+
     });
 
     $("#facebookbtn").click(function () {
-        console.log("signinbtn pressed");
+        console.log("facebookbtn pressed");
         document.querySelector('ons-navigator').resetToPage('splitter.html');
     });
 
     $("#googlebtn").click(function () {
-        console.log("signinbtn pressed");
+        console.log("googlebtn pressed");
         document.querySelector('ons-navigator').resetToPage('splitter.html');
     });
 
@@ -101,4 +110,31 @@ document.addEventListener('init', function (event) {
         document.querySelector('ons-navigator').resetToPage('splitter.html');
     });
 
+/////////////////////Append Resturant Card////////////////////////////////////
+    if (page.id === "Resturant") {
+        $.get("js/data.json", function (data, status) {
+            for (var index in data) {
+                var Res = data[index];
+                var Rescard = '<ons-card style="height : auto; margin-top:0px;" onclick="setID(`'+index+'`)"><ons-row>';
+                Rescard += '<ons-col width="25%"><img src=' + Res.img + ' alt="Onsen UI"style="width: 65px; height :55px;"></ons-col>';
+                Rescard += '<ons-col width="75%">';
+                Rescard += '<div style="font-size: 17px; white-space: nowrap;">&nbsp;&nbsp;<b>' + Res.ResturantName + '</b></div>';
+                Rescard += '<div style="color:grey">&nbsp;&nbsp;&nbsp;Distance : '+Res.Distance+'</div>';
+                Rescard += '<ons-row>';
+                Rescard += '<ons-col></ons-col>'; //for starrate
+                Rescard += '<ons-col width="25%"  style="text-align: right ;color:green;" ><b>'+Res.Status+'</b></ons-col>';
+                Rescard += '</ons-row></ons-col></ons-row></ons-card>'
+                console.log(Rescard);
+                $('#test').append(Rescard);
+            }
+        });
+    }
 });
+///////////////////////End of Append Resturant Card///////////////////////////////////////
+function setID(ID){
+    localStorage.setItem("selected",ID);
+    console.log(ID);
+}
+
+
+
